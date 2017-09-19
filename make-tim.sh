3a423fbca8743fc65b97cd269b897a6e86ab2c74
Ray <linxray@gmail.com>
2017-09-19 10:30:59 +0800

    delete user and group when base image has them

--- make-tim.sh
+++ make-tim.sh
@@ -7,7 +7,18 @@ stat $WHO > /dev/null || (echo You must mount a file to "$WHO" in order to prope
 USERID=$(stat -c %u $WHO)
 GROUPID=$(stat -c %g $WHO)
 
-deluser tim > /dev/null 2>&1
+deluser --remove-home tim > /dev/null 2>&1
+
+USERNAME=$(getent passwd "$USERID" | cut -d: -f1)
+if [ ! -z ${USERNAME} ]; then
+  deluser --remove-home $USERNAME > /dev/null 2>&1
+fi
+
+GROUPNAME=$(getent group "$GROUPID" | cut -d: -f1)
+if [ ! -z ${GROUPNAME} ]; then
+  delgroup $GROUPNAME > /dev/null 2>&1
+fi
+
 addgroup -g $GROUPID tim
 adduser -u $USERID -G tim -D -s /bin/sh tim
 
